3cb5cbfc7b3dd8e84aa44eae5e61e1131b69e01a
Ben Bai <benbai@thinkgeo.com>
2022-10-16 16:43:51 -0500

    Init the project.

---
+++ WindowWidthIssue/WindowWidthIssue/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowWidthIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WindowWidthIssue/WindowWidthIssue/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace WindowWidthIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void MainPage_OnLoaded(object sender, EventArgs e) 
+	{
+		if (this.Width < 0)
+			throw new Exception("Error");
+	}
+}
+
